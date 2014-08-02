a778a66cdb3771c39429875425ea1c897fe1afbd
Tomas Perez <tom@0x101.com>
2014-08-02 19:32:58 +0200

    Adding first version of the configuration for grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  // Load needed plugins
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
