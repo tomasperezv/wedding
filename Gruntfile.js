c14b0ab42eaa708cc28c5b3ad127761c27792a44
Tomas Perez <tom@0x101.com>
2014-08-02 20:20:38 +0200

    Adding tasks for grunt-contrib-clean.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,25 +2,61 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+
     pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      css: {
+        src: [
+          './static/css/*'
+        ],
+        dest : './tmp/css/svt.css'
+    },
+      js : {
+        src : [
+          './static/js/*'
+        ],
+        dest : './tmp/js/svt.js'
+      }
+    },
+
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        src: 'tmp/bundle.js',
+        dest: 'release/static/js/svt-<%= pkg.version %>.min.js'
       }
-    }
+    },
+
+    cssmin: {
+
+      minify: {
+        options: {
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
+        expand: true,
+        cwd: './tmp/css/',
+        src: ['*.css'],
+        dest: './release/static/css/',
+        ext: '-<%= pkg.version %>.min.css'
+      }
+
+    },
+
+    clean: ["./tmp/"]
+
   });
 
   // Load needed plugins
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'clean']);
 
 };
